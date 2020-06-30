35bd718e38515d2e82b58f0d85b5ab685770e7c5
zengjf <270806204@qq.com>
2020-06-30 15:30:45 +0800

    readme

--- Python-excel/dataAnalysis.py
+++ Python-excel/dataAnalysis.py
@@ -1,14 +1,18 @@
 # coding:utf-8
 # 导入读取Excel的库
-import xlrd
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # 导入需要读取Excel表格的路径
-data = xlrd.open_workbook(r'/Users/judezeng/Desktop/111.xls')
 
-# 计算行、列
-print(data.sheet_by_index(0).nrows)
-print(data.sheet_by_index(0).ncols)
+data = pd.read_excel('/Users/judezeng/Desktop/111.xls', sheet_name='销售明细')
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']  # 使图形能够正常显示中文
+
+print(data.tail(2))
+
+
+
+
 
 
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Awesome-Python
 ## python基础
-1、[python小练习30题](https://github.com/heyzeng/Awesome-Python/blob/master/Python-Basic/python30.md)
+- 1、[python小练习30题](https://github.com/heyzeng/Awesome-Python/blob/master/Python-Basic/python30.md)
+- 2、
